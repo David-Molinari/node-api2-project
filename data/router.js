dcf6050a0d1a6a924043d950bef1dd1bc1a9f5ea
David Molinari <Dmolina5@Davids-MBP-2.hsd1.pa.comcast.net>
2020-04-18 13:14:30 -0400

    Finished project

---
+++ data/router.js
@@ -0,0 +1,134 @@
+const express = require("express");
+
+const Hubs = require("./db.js");
+
+const router = express.Router();
+
+//3
+router.get("/", (req, res) => {
+  Hubs.find(req.query)
+    .then((hubs) => {
+      res.status(200).json({ queryString: req.query, hubs });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the posts",
+      });
+    });
+});
+
+//4
+router.get("/:id", (req, res) => {
+  Hubs.findById(req.params.id)
+    .then((hub) => {
+      if (hub) {
+        res.status(200).json(hub);
+      } else {
+        res.status(404).json({ message: "Post not found" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the post",
+      });
+    });
+});
+
+//1
+router.post("/", (req, res) => {
+  Hubs.insert(req.body)
+    .then((hub) => {
+      res.status(201).json(hub);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Error adding the post",
+      });
+    });
+});
+
+//6
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Hubs.remove(id)
+    .then((count) => {
+      if (count) {
+        res.status(200).json({ message: "post deleted" });
+      } else {
+        res.status(404).json({ message: "post not found" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+
+      res.status(500).json({ error: "something failed, sorry" });
+    });
+});
+
+//7
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+
+  console.log("changes:", changes);
+
+  Hubs.update(req.params.id, changes)
+    .then((count) => {
+      if (count) {
+        Hubs.findById(req.params.id)
+          .then((hub) => {
+            res.status(200).json(hub);
+          })
+          .catch((err) => {
+            res
+              .status(500)
+              .json({ errorMessage: "error reading the updated posted" });
+          });
+      } else {
+        res.status(404).json({ message: "The post could not be found" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating the post",
+      });
+    });
+});
+
+//5
+router.get("/:id/comments", (req, res) => {
+  Hubs.findPostComments(req.params.id)
+    .then((comment) => {
+      res.status(200).json(comment);
+    })
+    .catch((err) => {
+      res.status(500).json({ errorMessage: "error reading comments" });
+    });
+});
+
+//8
+router.get("/comments/:id", (req, res) => {
+    Hubs.findCommentById(req.params.id)
+      .then((comment) => {
+        res.status(200).json(comment);
+      })
+      .catch((err) => {
+        res.status(500).json({ errorMessage: "error reading comments" });
+      });
+  });
+
+//2
+router.post("/:id/comments", (req, res) => {
+  Hubs.insertComment(req.body)
+    .then((comment) => {
+      res.status(201).json(comment);
+    })
+    .catch((err) => {
+      res.status(500).json({ errorMessage: "error adding messages" });
+    });
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const postsRouter = require("./data/router.js"); // <-----=====
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/posts", postsRouter); // <--------=============
+
+// test it by making a GET request to localhost:4000/api/hubs
+
+server.get("/", (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+
+server.listen(4001, () => {
+  console.log("\n*** Server Running on http://localhost:4001 ***\n");
+});
